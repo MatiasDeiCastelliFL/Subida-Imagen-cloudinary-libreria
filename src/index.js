b0e37a2b34ffe641318ce835fe2612caff45c4fa
MatiasDeiCastelliFL <matiasdeicastellifl@gmail.com>
2024-05-16 18:46:09 -0300

    repair

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
 
 const cloudinary_upload = async (
   cloud_name_cloudinary,
   api_key_name_cloudinary,
   api_secret_name_cloudinary
 ) => {
-  cloudinary.config({
-    cloud_name: cloud_name_cloudinary,
-    api_key: api_key_name_cloudinary,
-    api_secret: api_secret_name_cloudinary,
-  });
+  try {
+    await cloudinary.config({
+      cloud_name: cloud_name_cloudinary,
+      api_key: api_key_name_cloudinary,
+      api_secret: api_secret_name_cloudinary,
+    });
+  } catch (error) {
+    throw new Error("Error configuring Cloudinary");
+  }
 };
 
-const upload_up_archive = async (functionCloudinary, archivo, opciones) => {
+const upload_up_archive = async (
+  cloud_name,
+  api_key,
+  api_secret,
+  archivo,
+  opciones
+) => {
   try {
-    const resultado = await functionCloudinary().v2.uploader.upload(
-      archivo,
-      opciones
-    );
+    await cloudinary_upload(cloud_name, api_key, api_secret);
+    const resultado = await cloudinary.uploader.upload(archivo, opciones);
     return resultado;
   } catch (error) {
     throw new Error("Error al subir el archivo a Cloudinary");
   }
 };
 
-module.exports = { upload_up_archive, cloudinary_upload };
+module.exports = { upload_up_archive };
